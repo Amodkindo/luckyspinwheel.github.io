bb904af6065f7a5ab576df5087e2f36132852fad
AMOD KINDO <amodkindo9905@gmal.com>
2024-02-20 21:36:56 +0530

    wheel

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// let wheel = document.querySelector('.wheel');
+// let inner = document.querySelector('.inner');
+// let spinBtn = document.querySelector('.spinBtn');
+
+// let value1 = Math.ceil(Math.random() * -3600);
+// let value2 = Math.ceil(Math.random() * 3600);
+
+// spinBtn.onclick = function () {
+//     wheel.style.transform = "rotate(" + value + "deg)";
+//     value1 += Math.ceil(Math.random() * -3600);
+// }
